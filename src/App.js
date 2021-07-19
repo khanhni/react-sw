6fb006d5c4519fd3fe3a380fd1db823a73a4887a
Khanh Nguyen <khanh.nguyen.quoc@vieon.vn>
2021-07-19 15:26:00 +0700

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const pushServerPublicKey = "BL9mTXkxuYardoFD2ZwnynR_XhLxgWyp_z2N1vaNDLTA-mwPXXMfHDfPVhuFuJp1KJn26obMnOW5EcNZ5WKRtoE"
 function App() {
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+  if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js').then(function(reg) {
+    console.log('Service Worker Registered!', reg);
+
+    navigator.serviceWorker.ready.then(function(ready) {
+      ready.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: pushServerPublicKey
+  });
+
+    ready.pushManager.getSubscription().then(function(sub) {
+      if (sub === null) {
+        // Update UI to ask user to register for Push
+        console.log('Not subscribed to push service!');
+      } else {
+        // We have a subscription, update the database
+        console.log('Subscription object: ', sub);
+      }
+    });
+    })
+
+    
+  })
+   .catch(function(err) {
+    console.log('Service Worker registration failed: ', err);
+  });
+}
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +52,14 @@ function App() {
       </header>
     </div>
   );
+}
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
 }
 
 export default App;
--- src/service-worker.js
+++ src/service-worker.js
@@ -69,4 +69,32 @@ self.addEventListener('message', (event) => {
   }
 });
 
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
+
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
+  e.waitUntil(
+    self.registration.showNotification('Hello world!', options)
+  );
+});
+
 // Any other custom service worker logic can go here.
